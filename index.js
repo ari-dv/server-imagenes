7dec7779a07eccad4642258fa7accabdf2642e24
ari-dv <rosalesariana102@gmail.com>
2026-04-25 01:03:56 -0500

    actualizado tamaño a 5mb

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ 
     storage: storage,
-    limits: { fileSize: 10 * 1024 * 1024 }
+    limits: { fileSize: 5 * 1024 * 1024 }
 }).single('image');
 
 app.use('/images', express.static(uploadDir));
